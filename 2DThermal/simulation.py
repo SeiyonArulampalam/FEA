28304f035350d6b365615157649b683b97a06791
Seiyon Arulampalam <sarulampalam3@gatech.edu>
2024-10-24 06:33:18 -0400

    simulation script update

--- 2DThermal/simulation.py
+++ 2DThermal/simulation.py
@@ -5,6 +5,7 @@ import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
 import niceplots
 import utils
+import plot_utils
 import triangle_element
 import line_element
 
@@ -13,24 +14,25 @@ np.set_printoptions(precision=4)
 
 """Run the 2D heat trasnfer simulation"""
 # * Flags
-apply_convection = False
-apply_dirichlet = False
+apply_convection = True
 open_gmsh = False
 
 # * Define model parameters
 a_xx = 5.0  # Thermal conductivitty [W/mC]
 a_yy = 5.0  # Thermal conductivitty [W/mC]
 if apply_convection == True:
-    beta = 40.0  # Heat trasnf. coefficent [W/m^2 C]
+    beta = 4.0  # Heat trasnf. coefficent [W/m^2 C]
+    q_hat = 10.0  # Heat flux on convection boundary
 else:
     beta = 0.0
+    q_hat = 0.0  # Heat flux on convection boundary
 T_ambient = 20.0  # Ambient temperature [C]
 u_hat = 300.0  # Temperature on dirichlet boundary [C]
-q_hat = 0.0  # Heat flux on convection boundary
+
 
 # * Generate the mesh
-height = 0.08  # Length of analysis domain [m]
-length = 0.02  # Hieght of analysis domain [m]
+height = 1  # Length of analysis domain [m]
+length = 1  # Hieght of analysis domain [m]
 mesh_info = utils.generate_mesh(lenght=length, height=height, open_gmsh=open_gmsh)
 
 # Extract the variables from the dictionary
@@ -56,9 +58,18 @@ n_elems = len(elemTags)
 n_nodes = len(nodeTags)
 
 # * Define boundary condition node tags
-dirichlet_bc_tags = nodeTags_s1.copy()
+# Case 1
+# dirichlet_bc_tags = []
+# convection_bc_tags = []
+# dirichlet_bc_tags.extend(nodeTags_s1)
+# dirichlet_bc_tags.extend(nodeTags_s2)
+# dirichlet_bc_tags.extend(nodeTags_s3)
+# dirichlet_bc_tags.extend(nodeTags_s4)
 
+# Case 2
+dirichlet_bc_tags = []
 convection_bc_tags = []
+dirichlet_bc_tags = nodeTags_s1.copy()
 convection_bc_tags.extend(nodeTags_s2[:])
 convection_bc_tags.extend(nodeTags_s3[1:])
 convection_bc_tags.extend(nodeTags_s4[1:])
@@ -68,7 +79,7 @@ print("Dirichlet BC tags:", dirichlet_bc_tags)
 
 # * Assemble FEA matrices
 # Define excitation for each element
-g = np.zeros(n_elems) + 10
+g = np.zeros(n_elems) + 100
 
 # Define handles for numerical integration on triangle elements
 wts_triangle, xi_eta_triangle = triangle_element.GaussOrder3()
@@ -110,6 +121,10 @@ K_analytical = utils.assemble_analytical_K(
 # Check is K and K_analytical are close
 if not np.allclose(K, K_analytical):
     raise ValueError("ERROR: K and K_analytical do not match")
+else:
+    print()
+    print("Assembly Check:")
+    print("PASSED ANALYTICAL TEST: K")
 
 # Assemble H
 H = utils.assemble_H(
@@ -122,6 +137,18 @@ H = utils.assemble_H(
     nodeCoords=nodeCoords,
     convection_bc_tags=convection_bc_tags,
 )
+H_analytical = utils.assemble_analytical_H(
+    beta=beta,
+    num_nodes=n_nodes,
+    nodeCoords=nodeCoords,
+    convection_bc_tags=convection_bc_tags,
+)
+
+# Check if H and H_analytical are close
+if not np.allclose(H, H_analytical):
+    raise ValueError("ERROR: H and H_analytical do not match")
+else:
+    print("PASSED ANALYTICAL TEST: H")
 
 # Assemble f
 f = utils.assemble_f(
@@ -145,16 +172,87 @@ f_analytical = utils.assemble_analytical_f(
     nodeCoords=nodeCoords,
 )
 
-# Check is K and K_analytical are close
+# Check if f and f_analytical are close
 if not np.allclose(f, f_analytical):
     raise ValueError("ERROR: f and f_analytical do not match")
+else:
+    print("PASSED ANALYTICAL TEST: f")
 
 # Assemble P
+P = utils.assemble_P(
+    wts=wts_line,
+    xi_pts=xi_line,
+    jac_func=jac_func_line,
+    shape_func_vals=shape_func_vals_line,
+    beta=beta,
+    T_ambient=T_ambient,
+    num_nodes=n_nodes,
+    nodeCoords=nodeCoords,
+    convection_bc_tags=convection_bc_tags,
+)
+
+P_analytical = utils.assemble_analytical_P(
+    beta=beta,
+    T_ambient=T_ambient,
+    num_nodes=n_nodes,
+    nodeCoords=nodeCoords,
+    convection_bc_tags=convection_bc_tags,
+)
+
+# Check if P and P_analytical are close
+if not np.allclose(P, P_analytical):
+    raise ValueError("ERROR: P and P_analytical do not match")
+else:
+    print("PASSED ANALYTICAL TEST: P")
 
 # Assemble Q
+Q = utils.assemble_Q(
+    wts=wts_line,
+    xi_pts=xi_line,
+    jac_func=jac_func_line,
+    shape_func_vals=shape_func_vals_line,
+    q_hat=q_hat,
+    num_nodes=n_nodes,
+    nodeCoords=nodeCoords,
+    convection_bc_tags=convection_bc_tags,
+)
+
+Q_analytical = utils.assemble_analytical_Q(
+    q_hat=q_hat,
+    num_nodes=n_nodes,
+    nodeCoords=nodeCoords,
+    convection_bc_tags=convection_bc_tags,
+)
+
+# Check if f and f_analytical are close
+if not np.allclose(Q, Q_analytical):
+    raise ValueError("ERROR: Q and Q_analytical do not match")
+else:
+    print("PASSED ANALYTICAL TEST: Q")
+
+# Left hand side matrix
+LHS_matrix = K + H
+RHS_vector = f + P + Q
+
+# * Apply dirichlet boundary conditions
+K_global, b_global = utils.apply_dirichlet_bc(
+    K_global=LHS_matrix.copy(),
+    b_global=RHS_vector.copy(),
+    numNodes=n_nodes,
+    nodesBC=dirichlet_bc_tags,
+    u_hat=u_hat,
+)
 
 # * Solve the steady state problem
+u = np.linalg.solve(K_global, b_global)
 
 # * Solve the transient simulation
 
 # * Visualize simulations
+plot_utils.contour_mpl(
+    xyz_nodeCoords=nodeCoords.reshape(-1, 3),
+    z=u,
+    title="fig",
+    fname="contour.jpg",
+    flag=False,
+)
